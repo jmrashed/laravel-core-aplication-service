e7930e9477ede6938370fefda86f6c3c916f7a88
Rashed Zaman <rashed@sookh.com>
2022-11-04 10:32:20 +0600

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,84 @@
         </div>
     </div>
     <div class="row installer-container">
+
+    <div class="col-4">
+            <div class="padding-left-top">
+                <img src="{{ $base_path  }}/images/Logo.png" alt="" />
+                <div class="mt-5 pe-2 follow-next-step-side" step-count="1">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="p-3 step-with-border completed rounded-circle d-flex flex-column justify-content-center align-items-center image-icon tab-button">
+                            <img src="{{ $base_path}}/images/check-mark.svg" alt="" />
+                        </div>
+                        <div>
+                            <p>01.</p>
+                            <h5><b>Welcome Note</b></h5>
+                        </div>
+                    </div>
+                    <span class="next-step-status-line"></span>
+                </div>
+                <div class="pe-4 follow-next-step-side" step-count="2">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="p-3 border step-with-border completed rounded-circle d-flex flex-column justify-content-center align-items-center image-icon tab-button">
+                            <img src="{{ $base_path}}/images/check-mark.svg" alt="" />
+                        </div>
+                        <div class="col-9">
+                            <p>02.</p>
+                            <h5><b>Check Environment</b></h5>
+                        </div>
+                    </div>
+                    <span class="next-step-status-line"></span>
+                </div>
+                <div class="pe-4 follow-next-step-side" step-count="3">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="border step-with-border completed  rounded-circle d-flex flex-column justify-content-center align-items-center image-icon tab-button">
+                            <img src="{{ $base_path}}/images/check-mark.svg" alt="" />
+                        </div>
+                        <div class="ps-2">
+                            <p>03.</p>
+                            <h5><b>Licence Verification</b></h5>
+                        </div>
+                    </div>
+                    <span class="next-step-status-line"></span>
+                </div>
+                <div class="pe-4 follow-next-step-side" step-count="4">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="p-3 border step-with-border completed rounded-circle d-flex flex-column justify-content-center align-items-center image-icon tab-button">
+                            <img src="{{ $base_path }}/images/check-mark.svg" alt="" />
+                        </div>
+                        <div class="col-9">
+                            <p>04.</p>
+                            <h5><b>Database Setup</b></h5>
+                        </div>
+                    </div>
+                    <span class="next-step-status-line"></span>
+                </div>
+                <div class="pe-4 follow-next-step-side" step-count="5">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="p-3 border step-with-border completed rounded-circle d-flex flex-column justify-content-center align-items-center image-icon tab-button">
+                            <img src="{{ $base_path }}/images/check-mark.svg" alt="" />
+                        </div>
+                        <div class="ps-2">
+                            <p>05.</p>
+                            <h5><b>Admin Setup</b></h5>
+                        </div>
+                    </div>
+                    <span class="next-step-status-line"></span>
+                </div>
+                <div class="pe-4 follow-next-step-side" step-count="6">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="p-3 border step-with-border initial rounded-circle d-flex flex-column justify-content-center align-items-center image-icon tab-button">
+                            <img src="{{ $base_path  }}/images/icon-white/complete.svg" alt="" />
+                        </div>
+                        <div>
+                            <p>06.</p>
+                            <h5><b>Complete</b></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
        @yield('content')
     </div>
     <script type="text/javascript" src="{{ $base_path}}/js/jquery-3.6.0.min.js"></script>
